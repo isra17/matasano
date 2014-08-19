0103403909ef6b450166791adf4f2e22d6444121
Israël Hallé <israel.halle@shopify.com>
2014-08-19 01:46:42 -0400

    Add challenge 3

--- set1.rs
+++ set1.rs
@@ -2,6 +2,8 @@ extern crate serialize;
 use serialize::hex::{FromHex, ToHex};
 use serialize::base64::{ToBase64, STANDARD};
 
+static LETTER_FREQUENCY: &'static [f32] = & [0.0855, 0.0160, 0.0316, 0.0387, 0.1210, 0.0218, 0.0209, 0.0496, 0.0733, 0.0022, 0.0081, 0.0421, 0.0253, 0.0717, 0.0747, 0.0207, 0.0010, 0.0633, 0.0673, 0.0894, 0.0268, 0.0106, 0.0183, 0.0019, 0.0172, 0.0011];
+
 fn hex_to_base64(hex : &str) -> String {
     let config = STANDARD;
     hex.from_hex().unwrap().as_slice().to_base64(config)
@@ -9,6 +11,28 @@ fn hex_to_base64(hex : &str) -> String {
 
 fn xor(v1 : &[u8], v2 : &[u8]) -> Vec<u8> {
     v1.iter().zip(v2.iter()).map(|(b1, b2)| *b1 ^ *b2).collect::<Vec<u8>>()
+}
+
+fn byte_decrypt(c : &[u8], k : u8) -> Vec<u8> {
+    c.iter().map(|b| b ^ k).collect::<Vec<u8>>()
+}
+
+fn score_message(m : &[u8]) -> f32 {
+    let mut letter_count : [uint, ..26] = [0, ..26];
+    let mut m_iter = m.iter().filter_map(|&b| {
+        match b {
+            65..90 => Some(b-65),
+            97..122 => Some(b-97),
+            _ => None
+        }
+    });
+    for b in m_iter {
+        letter_count[b as uint] += 1;
+    }
+    let message_freq = letter_count.iter().map(|&f| f as f32/m.len() as f32);
+    message_freq.zip(LETTER_FREQUENCY.iter()).fold(0f32, |score, (m_freq, english_freq)| {
+        score + 1f32/std::num::abs(english_freq - m_freq)
+    })
 }
 
 fn ch1() {
@@ -25,7 +49,27 @@ fn ch2() {
     assert!("746865206b696420646f6e277420706c6179" == xor_vec.as_slice().to_hex().as_slice());
 }
 
+fn ch3() {
+    let c = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".from_hex().unwrap();
+    let mut best_message : Vec<u8> = vec!();
+    let mut best_message_score = 0f32;
+    let mut key = 0;
+    for k in std::iter::range_inclusive(0, 255) {
+        let message = byte_decrypt(c.as_slice(), k);
+        let message_score = score_message(message.as_slice());
+        if message_score > best_message_score {
+            best_message = message;
+            best_message_score = message_score;
+            key = k;
+        }
+    }
+
+    println!("Key: {}", [key].to_hex());
+    println!("Message: {}", String::from_utf8(best_message).unwrap());
+}
+
 fn main() {
     ch1();
     ch2();
+    ch3();
 }
