1fc545e0aeb25c8860be5515702e953bfe1a4a4a
Israël Hallé <israel.halle@shopify.com>
2014-08-22 19:56:09 -0400

    Add challenge 11

--- set2.rs
+++ set2.rs
@@ -2,9 +2,10 @@ extern crate openssl;
 extern crate serialize;
 
 use serialize::base64::{FromBase64};
+use std::rand::{task_rng, Rng};
 use std::io::BufferedReader;
 use std::io::File;
-use openssl::crypto::symm::{decrypt, AES_128_ECB};
+use openssl::crypto::symm::{encrypt, decrypt, AES_128_ECB, AES_128_CBC};
 
 
 fn xor(v1 : &[u8], v2 : &[u8]) -> Vec<u8> {
@@ -41,6 +42,33 @@ fn decrypt_aes_cbc(k : &[u8], c : &[u8], iv : &[u8]) -> Vec<u8> {
     }).collect::<Vec<u8>>()
 }
 
+
+fn gen_key() -> Vec<u8> {
+    let mut key = [0u8, ..16];
+    task_rng().fill_bytes(key);
+    Vec::from_slice(key)
+}
+
+fn gen_random_size_vec(min : uint, max : uint) -> Vec<u8> {
+    let size = task_rng().gen_range(min, max);
+    let mut vec = Vec::new();
+    vec.grow(size, &(0));
+    task_rng().fill_bytes(vec.as_mut_slice());
+    vec
+}
+
+fn crypto_service(input : &[u8]) -> Vec<u8> {
+    let key = gen_key();
+    let iv = gen_key();
+    let prepend_bytes = gen_random_size_vec(5, 10);
+    let append_bytes = gen_random_size_vec(5, 10);
+    let data = prepend_bytes + input + append_bytes;
+    let choices = [AES_128_ECB, AES_128_CBC];
+    let t = task_rng().choose(choices).unwrap();
+    println!("Used {}", (match *t { AES_128_ECB => "ECB", AES_128_CBC => "CBC", _ => "Unknown" }));
+    encrypt(*t, key.as_slice(), iv, data.as_slice())
+}
+
 fn ch9() {
     println!("------- 9 ---------");
     let mut data = Vec::from_slice("YELLOW SUBMARINE".as_bytes());
@@ -62,7 +90,20 @@ fn ch10() {
     println!("Message: {}", bytes_to_string(m));
 }
 
+fn ch11() {
+    for _ in range(0u32, 10u32) {
+        let input = ['a' as u8, ..64];
+        let data = crypto_service(input);
+        if data.slice(16, 32) == data.slice(32, 48) {
+            println!("Guess ECB");
+        } else {
+            println!("Guess CBC");
+        }
+    }
+}
+
 fn main() {
     ch9();
     ch10();
+    ch11();
 }
